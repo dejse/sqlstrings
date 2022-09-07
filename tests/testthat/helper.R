67fdcf800bca27c8ff27e6d09b47d689d1cc62ae
wiwirebell <47352728+wiwirebell@users.noreply.github.com>
2022-09-07 11:29:56 +0200

    write test files to session tmp folder

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,36 +1,35 @@
 ### Helper Functions ### 
-create_test_files <- function() {
-  fs::dir_create("./test-folder")
-  fs::dir_create("./test-folder2")
-  fs::file_create("./test-folder/file.txt")
-  fs::file_create("./test-folder/file2.txt")
+create_test_files <- function(tmp = tempdir(check = TRUE)) {
+  fs::dir_create(fs::path(tmp, "test-folder"))
+  fs::dir_create(fs::path(tmp, "test-folder2"))
+  fs::file_create(fs::path(tmp, "test-folder", "file.txt"))
+  fs::file_create(fs::path(tmp, "test-folder", "file2.txt"))
 }
 
-add_sql_code <- function() {
-  readr::write_lines("-- name: create
+add_sql_code <- function(tmp = tempdir(check = TRUE)) {
+  readr::write_lines("
+  -- name: create
   create table tab1 (
     id integer primary key,
     city text unique not null,        -- comment
     pop integer
   );
-  ", "./test-folder/file.txt", append = TRUE)
-  readr::write_lines("-- name: select
-    select * from tab1;
-  ", "./test-folder/file.txt", append = TRUE)
+  -- name: select
+  select * from tab1;
+  ", fs::path(tmp, "test-folder", "file.txt"))
 
-  readr::write_lines("-- name: insert
-    insert into tab1 values 
-      (1, 'Berlin', 3),
-      (2, 'Paris', 2),
-      (3, 'London', 8);
-  ", "./test-folder/file2.txt", append = TRUE)
-
-  readr::write_lines("-- name: select_count
-    select count(*) from tab1;
-  ", "./test-folder/file2.txt", append = TRUE)
+  readr::write_lines("
+  -- name: insert
+  insert into tab1 values 
+    (1, 'Berlin', 3),
+    (2, 'Paris', 2),
+    (3, 'London', 8);
+  -- name: select_count
+  select count(*) from tab1;
+  ", fs::path(tmp, "test-folder", "file2.txt"))
 }
 
-delete_test_files <- function() {
-  fs::dir_delete("./test-folder")
-  fs::dir_delete("./test-folder2")
+delete_test_files <- function(tmp = tempdir(check = TRUE)) {
+  fs::dir_delete(fs::path(tmp, "test-folder"))
+  fs::dir_delete(fs::path(tmp, "test-folder2"))
 }
--- tests/testthat/test-sqlstrings.R
+++ tests/testthat/test-sqlstrings.R
@@ -1,23 +1,25 @@
 test_that("Parsing paths work correctly", {
   # before
-  create_test_files()
+  tmp <- tempdir(check = TRUE)
+  create_test_files(tmp)
 
   # tests
   expect_error(generate_sql_strings("./does-not-exist/"))
   expect_error(generate_sql_strings("./does-not-exist/file2.txt"))
   expect_error(generate_sql_strings())
-  expect_equal(generate_sql_strings("./test-folder2"), list())
+  expect_equal(generate_sql_strings(fs::path(tmp, "test-folder2")), list())
 
   # after
-  delete_test_files()
+  delete_test_files(tmp)
 })
 
 
 test_that("Parsing SQL Code works correctly", {
   # before
-  create_test_files()
-  add_sql_code()
-  s <- generate_sql_strings("./test-folder")
+  tmp <- tempdir(check = TRUE)
+  create_test_files(tmp)
+  add_sql_code(tmp)
+  s <- generate_sql_strings(fs::path(tmp, "test-folder"))
 
   # tests
   expect_true("create" %in% names(s))
@@ -26,9 +28,9 @@ test_that("Parsing SQL Code works correctly", {
   expect_true("select_count" %in% names(s))
   expect_equal(s$create, "create table tab1 (\n    id integer primary key,\n    city text unique not null,        -- comment\n    pop integer\n  );")
   expect_equal(s$select, "select * from tab1;")
-  expect_equal(s$insert, "insert into tab1 values \n      (1, 'Berlin', 3),\n      (2, 'Paris', 2),\n      (3, 'London', 8);")
+  expect_equal(s$insert, "insert into tab1 values \n    (1, 'Berlin', 3),\n    (2, 'Paris', 2),\n    (3, 'London', 8);")
   expect_equal(s$select_count, "select count(*) from tab1;")
 
   # after
-  delete_test_files()
+  delete_test_files(tmp)
 })
